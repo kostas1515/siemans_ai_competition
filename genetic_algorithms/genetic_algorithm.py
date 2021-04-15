0064e032d372e0c451a7af5b7e07fa387cfaaeb1
kostas1515 <konsa15@liverpool.ac.uk>
2021-04-15 11:03:02 +0100

    move genetic algorithms work to subfolder

--- genetic_algorithms/genetic_algorithm.py
+++ genetic_algorithms/genetic_algorithm.py
@@ -6,7 +6,7 @@ import os
 
 root="./data"
 # datasets = ["trainingdata_a.csv","trainingdata_b.csv","trainingdata_c.csv"]
-datasets = ["trainingdata_b.csv"]
+datasets = ["trainingdata_a.csv"]
 for dataset in datasets:
     checkpoint_folder=os.path.join('checkpoints',dataset.split('.')[0])
     if not os.path.exists(checkpoint_folder):
@@ -15,7 +15,7 @@ for dataset in datasets:
     data_train=pd.read_csv(path)
     x_train=np.array([data_train['x_i1'],data_train['x_i2']]).T
     y_train=np.array([data_train['l_i']]).T.ravel()
-    pipeline_optimizer = TPOTClassifier(generations=30, population_size=100, cv=10,
+    pipeline_optimizer = TPOTClassifier(generations=100, population_size=100, cv=10,
                                          verbosity=2,scoring='f1',n_jobs=-1,periodic_checkpoint_folder=checkpoint_folder)
     pipeline_optimizer.fit(x_train, y_train)
     pipeline_optimizer.export(checkpoint_folder+"_pipeline_final.py")
